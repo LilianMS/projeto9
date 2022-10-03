2f2bab1c0b95bdd4559ad45f9fe0fab095935416
LilianMS <100161978+LilianMS@users.noreply.github.com>
2022-10-03 18:54:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+//variáveis globais
+var box;
+
+
+function setup(){
+    createCanvas(600,600);
+    box = createSprite(200,180,20,20);
+}
+
+
+function draw(){
+    background("cyan");
+    if(keyDown("left")){
+        box.x = box.x - 5;
+        background("red");
+    }
+    if(keyDown("right")){
+        box.x = box.x + 5;
+        background("blue");
+    }
+    if(keyDown("up")){
+        box.y = box.y - 5;
+        background("green");
+    }
+    if(keyDown("down")){
+        box.y = box.y + 5;
+        background("yellow");
+    }
+
+    drawSprites();
+}
